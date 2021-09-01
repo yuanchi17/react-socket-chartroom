bd6189317ebacb83f465ed08af49f1309acb6594
李元琦 <yuan.chi@program.com.tw>
2021-09-01 18:20:47 +0800

    express/client

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-// // Serve static files from the React frontend app
-// app.use(express.static(path.join(__dirname, 'client/build')))
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-// // Anything that doesn't match the above, send back index.html
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
-// })
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.get("/api/getText", cors(), async (req, res, next) => {
   try {

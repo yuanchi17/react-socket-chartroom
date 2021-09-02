415dd5322613078f816b94268881740fbd5d91ce
李元琦 <yuan.chi@program.com.tw>
2021-09-02 11:05:58 +0800

    fix express/client

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const app = express()
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
-
 app.get("/api/getText", cors(), async (req, res, next) => {
   try {
     console.log('Welcome')
@@ -21,6 +16,10 @@ app.get("/api/getText", cors(), async (req, res, next) => {
   }
 });
 
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

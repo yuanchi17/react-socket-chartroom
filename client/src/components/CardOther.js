2983b9303e553d47eb5b681bb6b51ae1084d0129
李元琦 <yuan.chi@program.com.tw>
2021-09-16 17:56:14 +0800

    update

--- client/src/components/CardOther.js
+++ client/src/components/CardOther.js
@@ -1,20 +1,37 @@
 import React from 'react'
-// TODO: 離線的標示
+
 const CardOther = ({ member }) => {
   return (
-    <div className="member-other m-2 p-2">
-      <div className="other-img-area d-flex">
-        <img
-          className="img-fluid my-auto"
-          src={`https://i.imgur.com/${member.img}.png`}
-          alt="other"
-        ></img>
-      </div>
-      <div className="other-intro-area ml-2 text-left">
-        <p className="m-0 name">{member.name}</p>
-        <span className="text-wrap text-muted intro">{member.intro}</span>
-      </div>
-    </div>
+    <>
+      {member.connect
+        ? <div className="member-other m-2 p-2">
+            <div className="other-img-area d-flex">
+              <img
+                className="img-fluid my-auto"
+                src={`https://i.imgur.com/${member.img}.png`}
+                alt="other"
+              ></img>
+            </div>
+            <div className="other-intro-area ml-2 text-left">
+              <p className="m-0 name">{member.name}</p>
+              <span className="text-wrap text-muted intro">{member.intro}</span>
+            </div>
+          </div>
+        : <div className="member-other m-2 p-2 leave-member">
+            <div className="other-img-area d-flex">
+              <img
+                className="img-fluid my-auto"
+                src={`https://i.imgur.com/${member.img}.png`}
+                alt="other"
+              ></img>
+            </div>
+            <div className="other-intro-area ml-2 text-left">
+              <p className="m-0 name">{member.name}</p>
+              <span className="text-wrap text-muted intro">{member.intro}</span>
+            </div>
+          </div>
+      }
+    </>
   )
 }
 
